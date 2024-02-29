28c1375dec26be37d2b298a08a3ab2e533cf483c
Ishigami100 <yamahaya5151@icloud.com>
2024-02-29 16:09:00 +0900

    migrationのエラーを直す

--- db/migrate/20240229042844_create_quizzes.rb
+++ db/migrate/20240229042844_create_quizzes.rb
@@ -1,10 +1,11 @@
 class CreateQuizzes < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :quizzes do |t|
       t.references :user, foreign_key: true, null: false
       t.integer :pokemon_id , null: false
       t.integer :challenge_upper_limit , null: false
 
       t.timestamps
+    end
   end
 end
