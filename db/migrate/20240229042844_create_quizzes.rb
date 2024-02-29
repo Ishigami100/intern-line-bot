8224aedc0a6f70d8d927839dc218ff0e15d68fa7
Ishigami100 <yamahaya5151@icloud.com>
2024-02-29 14:07:15 +0900

    外部キーを張る

--- db/migrate/20240229042844_create_quizzes.rb
+++ db/migrate/20240229042844_create_quizzes.rb
@@ -2,7 +2,7 @@ class CreateQuizzes < ActiveRecord::Migration[6.0]
   def change
     create_table :users , id: false do |t|
       t.column :quiz_id, 'INT PRIMARY KEY AUTO_INCREMENT', null: false
-      t.integer :user_id, null: false
+      t.references :user, foreign_key: true, null: false
       t.integer :pokemon_id , null: false
       t.integer :challenge_upper_limit , null: false
 
--- db/migrate/20240229042852_create_answers.rb
+++ db/migrate/20240229042852_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.column :answer_id, 'INT PRIMARY KEY AUTO_INCREMENT', null: false
-      t.integer :quiz_id, null: false
+      t.references :quiz, foreign_key: true, null: false
       t.string :answer_text null: false
       t.boolean :answer_succeed, null: false
 
