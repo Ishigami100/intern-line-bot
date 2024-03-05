7ad46e6cbe877db5d6490b1ac275f5385827da7d
Ishigami100 <yamahaya5151@icloud.com>
2024-03-05 11:41:07 +0900

    validationを直す

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,33 +1,34 @@
 class Answer < ApplicationRecord
     belongs_to :quiz
 
-    def insert_answer(user_line_id,input_text)
-        user_id = User.find_by!(user_line_id: user_line_id).id
-        quiz=Quiz.find_by!(user_id: user_id).order(updated_at: :desc).limit(1)
-        Answer.create(quiz_id: quiz.id, answer_text: input_text, answer_succeed: is_answer_succeed?(input_text))
+    def self.insert_answer(user_line_id,input_text)
+        now_quiz_id = Quiz.new.get_now_quiz(user_line_id)
+        Answer.create(quiz_id: now_quiz_id, answer_text: input_text, answer_succeed: is_answer_succeed?(input_text))
     end
 
     def one_quiz_to_answer_num(user_line_id)
-        user_id = User.find_by!(user_line_id: user_line_id).id
-        now_quiz_id=Quiz.where(user_id: user_id).order(updated_at: :desc).limit(1).id
+        now_quiz_id = Quiz.new.get_now_quiz(user_line_id).id
         Answer.where(quiz_id: now_quiz_id).count
     end
 
     private 
 
     #送られた答えに漢字もしくはひらがなが含まれていた場合は、かな変換し、正しいかを確認するメソッド
+
     def is_answer_succeed?(input_text)
-        return false if is_kanji? input_text 
-        if is_kana? input_text
-            text=to_kana(text)
-        end
-        #APIを呼び出し日本語名と一致するかを確認する
+        return false if validate_text?(input_text)
         answer_text = "ピカチュウ" #仮
         if  text == answer_text
             true
         else
             false
         end
+    end
+
+    def validate_text?(input_text)
+        return false if is_katakana?
+        #ここにvalidatonを追記する
+        return true
     end
 
     #ひら→カタ
@@ -46,4 +47,11 @@ class Answer < ApplicationRecord
         return true if text =~ /^[一-龥]+$/
         false
     end
+
+    #カタカナ？
+    def is_katakana?(text)
+        return true if text =~ /\A[ァ-ヶー－]+\z/
+        false
+    end
+        
 end
