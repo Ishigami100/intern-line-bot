ce0f52565ab4819613153a9c1cf893ee81c1c469
Ishigami100 <yamahaya5151@icloud.com>
2024-03-05 14:05:10 +0900

    クラスメソッドとインスタンスメソッドの使い分け

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,9 @@ class Answer < ApplicationRecord
     belongs_to :quiz
 
     def self.insert_answer(user_line_id,input_text)
-        now_quiz_id = Quiz.new.get_now_quiz(user_line_id)
-        Answer.create(quiz_id: now_quiz_id, answer_text: input_text, answer_succeed: is_answer_succeed?(input_text))
+        now_quiz_id = Quiz.new.get_now_quiz(user_line_id).id
+        Answer.create(quiz_id: now_quiz_id, answer_text: input_text, answer_succeed: self.is_answer_succeed?(input_text))
+        self.is_answer_succeed?(input_text)
     end
 
     def one_quiz_to_answer_num(user_line_id)
@@ -15,7 +16,7 @@ class Answer < ApplicationRecord
 
     #送られた答えに漢字もしくはひらがなが含まれていた場合は、かな変換し、正しいかを確認するメソッド
 
-    def is_answer_succeed?(input_text)
+    def self.is_answer_succeed?(input_text)
         return false if validate_text?(input_text)
         answer_text = "ピカチュウ" #仮
         if  text == answer_text
@@ -25,31 +26,31 @@ class Answer < ApplicationRecord
         end
     end
 
-    def validate_text?(input_text)
-        return false if is_katakana?
+    def self.validate_text?(input_text)
+        return false if is_katakana?(input_text)
         #ここにvalidatonを追記する
         return true
     end
 
     #ひら→カタ
-    def to_kana(text)
+    def self.to_kana(text)
         text.tr('ぁ-ん','ァ-ン')
     end
 
     #ひらがな？
-    def is_kana?(text)
+    def self.is_kana?(text)
         return true if text =~ /\A[ぁ-んー－]+\z/
         false
     end
 
     #漢字？
-    def is_kanji?(text)
+    def self.is_kanji?(text)
         return true if text =~ /^[一-龥]+$/
         false
     end
 
     #カタカナ？
-    def is_katakana?(text)
+    def self.is_katakana?(text)
         return true if text =~ /\A[ァ-ヶー－]+\z/
         false
     end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -5,19 +5,20 @@ class Quiz < ApplicationRecord
     belongs_to :user 
     has_many :answers
 
-    def self.insert_quiz(user_line_id)
-        user_id = User.find_by!(user_line_id: user_line_id).id
+    def self.insert_quiz(line_user_id)
+        p line_user_id
+        user_id = User.find_by!(line_user_id: line_user_id).id
         Quiz.create(user_id: user_id, pokemon_id: random_pokemon_id, challenge_upper_limit: CHALLENGE_UPPER_LIMIT)
     end
 
-    def get_now_quiz(user_line_id)
-        user_id = User.find_by!(user_line_id: user_line_id).id
+    def get_now_quiz(line_user_id)
+        user_id = User.find_by!(line_user_id: line_user_id).id
         now_quiz_id=Quiz.where(user_id: user_id).first
     end
 
     private 
 
-    def random_pokemon_id
+    def self.random_pokemon_id
         rand(1..MAX_POKEMON_ID)#ポケモンの個体数に依存する
     end
 
