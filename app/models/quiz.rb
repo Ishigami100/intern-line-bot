197abc7bcf81932c118191bcc05684baa0841f0e
Ishigami100 <yamahaya5151@icloud.com>
2024-03-05 11:41:29 +0900

    現在のクイズを取得するメソッドを追記する

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -5,9 +5,14 @@ class Quiz < ApplicationRecord
     belongs_to :user 
     has_many :answers
 
-    def insert_quiz(user_line_id)
+    def self.insert_quiz(user_line_id)
         user_id = User.find_by!(user_line_id: user_line_id).id
         Quiz.create(user_id: user_id, pokemon_id: random_pokemon_id, challenge_upper_limit: CHALLENGE_UPPER_LIMIT)
+    end
+
+    def get_now_quiz(user_line_id)
+        user_id = User.find_by!(user_line_id: user_line_id).id
+        now_quiz_id=Quiz.where(user_id: user_id).first
     end
 
     private 
