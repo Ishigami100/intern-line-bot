81d0e7265ffb9fe6942c6e806f77d86ce5eaca47
Ishigami100 <yamahaya5151@icloud.com>
2024-03-06 17:51:03 +0900

    vipsをインストールする

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,22 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+apt-get update
+apt-get install -y \
+curl \
+build-essential pkg-config libglib2.0-dev libexpat1-dev \
+libspng-dev
+
+pip install --upgrade pip
+pip install --user ninja meson
+
+curl -OL https://github.com/libvips/libvips/releases/download/v8.15.1/vips-8.15.1.tar.xz
+tar xf vips-8.15.1.tar.xz
+cd vips-8.15.1
+meson setup build-dir --prefix=/usr
+
+cd build-dir
+ninja
+ninja test
+ninja install
